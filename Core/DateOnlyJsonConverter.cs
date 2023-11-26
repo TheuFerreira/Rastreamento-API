4ab3e1f6a950e085c4175a47bbe31a82cafd17ce
Caique0Araujo <caique.ara19@gmail.com>
2023-11-26 10:25:42 -0300

    Add DateOnly converter to main

---
+++ Core/DateOnlyJsonConverter.cs
@@ -0,0 +1,19 @@
+﻿    using System.Text.Json.Serialization;
+    using System.Text.Json;
+
+namespace Core
+{
+    public sealed class DateOnlyJsonConverter : JsonConverter<DateOnly>
+    {
+        public override DateOnly Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return DateOnly.FromDateTime(reader.GetDateTime());
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateOnly value, JsonSerializerOptions options)
+        {
+            var isoDate = value.ToString("O");
+            writer.WriteStringValue(isoDate);
+        }
+    }
+}
