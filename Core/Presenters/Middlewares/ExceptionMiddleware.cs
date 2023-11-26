2029c67398f7ecf0d95355744420242c7d1f04c7
Matheus Ferreira <matheus.44300@gmail.com>
2023-11-25 21:45:20 -0300

    Added ExceptionMiddleware

---
+++ Core/Presenters/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Core.Domain.Exceptions;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Authentication;
+
+namespace Core.Presenters.Middlewares
+{
+    public class ExceptionMiddleware : IExceptionFilter, IFilterMetadata
+    {
+        public void OnException(ExceptionContext context)
+        {
+            Exception exception = context.Exception;
+
+            if (exception is InvalidCredentialException)
+            {
+                BadRequestObjectResult res = new(exception.Message)
+                {
+                    StatusCode = StatusCodes.Status401Unauthorized
+                };
+
+                context.Result = res;
+                return;
+            }
+
+            if (exception is NotFoundException)
+            {
+                BadRequestObjectResult res = new(exception.Message)
+                {
+                    StatusCode = StatusCodes.Status404NotFound
+                };
+
+                context.Result = res;
+                return;
+            }
+
+            BadRequestObjectResult badrequest = new(exception.Message)
+            {
+                StatusCode = 500
+            };
+
+            context.Result = badrequest;
+        }
+    }
+}
