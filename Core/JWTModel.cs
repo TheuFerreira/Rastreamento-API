6a9847de5a4f088ba115d212ac65d3df5a1da6f0
Matheus Ferreira <matheus.44300@gmail.com>
2023-11-25 21:33:47 -0300

    Added JWT

---
+++ Core/JWTModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Core
+{
+    public class JWTModel
+    {
+        public string Key { get; set; } = string.Empty;
+        public int Expiration { get; set; }
+    }
+}
--- Core/Program.cs
+++ Core/Program.cs
@@ -1,19 +1,40 @@
+using Core;
 using Core.Domain.Repositories;
 using Core.Infra.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using MySqlConnector;
 using System.Data;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+JWTModel jwt = builder.Configuration.GetSection("JWT").Get<JWTModel>();
 
 string connectionString = builder.Configuration.GetConnectionString("MySQL");
 IDbConnection connection = new MySqlConnection(connectionString);
 
 builder.Services.AddSingleton<IDbConnection>(connection);
+builder.Services.AddSingleton<JWTModel>(jwt);
 
 // Repositories
 builder.Services.AddSingleton<IUserRepository, UserRepository>();
 
 // Add services to the container.
+
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+         options.TokenValidationParameters = new TokenValidationParameters
+         {
+             ValidateIssuer = false,
+             ValidateAudience = false,
+             ValidateLifetime = true,
+             ValidateIssuerSigningKey = true,
+             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwt.Key)),
+             ClockSkew = TimeSpan.Zero
+         }
+    );
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
