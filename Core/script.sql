9a38158b87df6377c0a1b1a31dfe891235ea776a
Matheus Ferreira <matheus.44300@gmail.com>
2023-11-29 21:11:01 -0300

    Updated script

--- Core/script.sql
+++ Core/script.sql
@@ -11,3 +11,20 @@ CREATE TABLE users(
 	created_at DATETIME NOT NULL,
 	updated_at DATETIME NOT NULL
 );
+
+CREATE TABLE delivery (
+	id_delivery BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	id_user INT NOT NULL,
+	description VARCHAR(200) NOT NULL,
+	origin VARCHAR(200) NOT NULL,
+	destiny VARCHAR(200) NOT NULL,
+	observation VARCHAR(200) NULL,
+	code VARCHAR(16) NOT NULL
+);
+
+CREATE TABLE user_has_delivery(
+	id_delivery INT NOT NULL,
+	id_user INT NOT NULL,
+
+	PRIMARY KEY (id_delivery, id_user)
+);
