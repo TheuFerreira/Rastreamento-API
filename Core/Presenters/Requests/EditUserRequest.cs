bf2267ff85f5be67d68192d7528b797c669a0d55
Matheus Ferreira <matheus.44300@gmail.com>
2023-12-05 23:22:39 -0300

    Removed Warnings

--- Core/Presenters/Requests/EditUserRequest.cs
+++ Core/Presenters/Requests/EditUserRequest.cs
@@ -1,11 +1,15 @@
-﻿namespace Core.Presenters.Requests
+﻿using System.Text.Json.Serialization;
+
+namespace Core.Presenters.Requests
 {
     public class EditUserRequest
     {
         public int Id { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-        public string Fullname { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Fullname { get; set; } = string.Empty;
+
+        [JsonPropertyName("birth_date")]
         public DateOnly BirthDate { get; set; }
     }
 }
